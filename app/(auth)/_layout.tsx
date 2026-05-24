84a31438db18b040eaea7d41d42d84eb587ff5dd
thanhphucnguyen1810 <thanhphuc1810.work@gmail.com>
2026-05-24 15:12:49 +0700

    feat(navigation): setup routing architecture with Expo Router

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,16 @@
+import { useTheme } from '@/src/theme';
+import { Stack } from 'expo-router';
+
+export default function AuthLayout() {
+  const { c } = useTheme();
+
+  return (
+    <Stack screenOptions={{ 
+        headerShown: false, // Ẩn header mặc định để dùng AppHeader tự code
+        contentStyle: { backgroundColor: c.bg } // Set màu nền trắng
+    }}>
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+    </Stack>
+  );
+};
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,69 @@
+import { useTheme } from '@/src/theme';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  const { c, typography } = useTheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false, // Ẩn header trên cùng
+        tabBarActiveTintColor: c.primary, // Khi bấm vào tab thì icon đổi sang Đỏ
+        tabBarInactiveTintColor: c.textSub, // Tab không chọn thì màu xám
+        tabBarStyle: {
+          backgroundColor: c.bg,
+          borderTopWidth: 1,
+          borderTopColor: c.border,
+        },
+        tabBarLabelStyle: {
+          ...typography.variants.caption,
+        },
+      }}
+    >
+      {/* Tab 1: Trang chủ */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Trang chủ',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home-variant-outline" size={24} color={color} />
+          ),
+        }}
+      />
+      
+      {/* Tab 2: Khóa học */}
+      <Tabs.Screen
+        name="courses"
+        options={{
+          title: 'Khóa học',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="book-open-variant" size={24} color={color} />
+          ),
+        }}
+      />
+
+      {/* Tab 3: Thi */}
+      <Tabs.Screen
+        name="exams"
+        options={{
+          title: 'Thi',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="file-document-edit-outline" size={24} color={color} />
+          ),
+        }}
+      />
+
+      {/* Tab 4: Tài khoản */}
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Tài khoản',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account-circle-outline" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
